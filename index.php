fb3165d33ff5f4efcb715a1a189d3a51d602ec71
Ibon Azkoitia <ibon@kreatidos.com>
2023-12-20 15:10:36 +0100

    Crear clase 'Mobile - Fixed bottom nav'

--- index.php
+++ index.php
@@ -435,12 +435,37 @@
     </div>
 </main>
 
+<!-- Mapa -->
 <div class="fixed w-full flex justify-center bottom-20 z-50">
     <button class="py-2 px-5 text-sm text-white bg-[#222222] rounded-full flex items-center gap-2">
         Mapa
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 16px; width: 16px; fill: rgb(255, 255, 255);"><path d="M31.25 3.75a2.29 2.29 0 0 0-1.01-1.44A2.29 2.29 0 0 0 28.5 2L21 3.67l-10-2L2.5 3.56A2.29 2.29 0 0 0 .7 5.8v21.95a2.28 2.28 0 0 0 1.06 1.94A2.29 2.29 0 0 0 3.5 30L11 28.33l10 2 8.49-1.89a2.29 2.29 0 0 0 1.8-2.24V4.25a2.3 2.3 0 0 0-.06-.5zM12.5 25.98l-1.51-.3L9.5 26H9.5V4.66l1.51-.33 1.49.3v21.34zm10 1.36-1.51.33-1.49-.3V6.02l1.51.3L22.5 6h.01v21.34z"></path></svg>
     </button>
 </div>
+<!-- end Mapa -->
+
+<!-- Footer Nav -->
+<div class="fixed w-full py-3 border-t border-gray-300 bg-white flex justify-center items-center gap-8 bottom-0 z-50">
+    <a href="#" class="text-red-500 text-xs font-semibold">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-7 h-7 mx-auto">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
+        Explora
+    </a>
+    <a href="#" class="text-gray-500 text-xs font-semibold">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-7 h-7 mx-auto">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+        </svg>
+        Favoritos
+    </a>
+    <a href="#" class="text-gray-500 text-xs font-semibold">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-7 h-7 mx-auto">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        </svg>
+        Iniciar sesión
+    </a>
+</div>
+<!-- end Footer Nav -->
 
 <?php require 'components/footer.php'; ?>
 </body>
