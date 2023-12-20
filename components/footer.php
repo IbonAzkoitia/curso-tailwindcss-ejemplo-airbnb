245dfa43d46caa27c83af30807c85acca2ce8d13
Ibon Azkoitia <ibon@kreatidos.com>
2023-12-20 12:04:46 +0100

    Crear clase 'Mobile - Inspiracion'

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,122 @@
+<?php
+    $escapadas_tipos = [
+        'Más buscados',
+        'Ciudades',
+        'Playas',
+        'Con historia',
+        'Islas',
+        'Montañas',
+        'Categorías',
+    ];
+
+    $escapadas = [
+        [
+            'titulo' => 'Amsterdam',
+            'texto' => 'Villas en alquiler',
+        ],
+	    [
+		    'titulo' => 'Barcelona',
+		    'texto' => 'Alquileres vacacionales',
+	    ],
+	    [
+		    'titulo' => 'Benasque',
+		    'texto' => 'Alquileres vacacionales',
+	    ],
+	    [
+		    'titulo' => 'Bordeaux',
+		    'texto' => 'Apartamentos en alquiler',
+	    ],
+	    [
+		    'titulo' => 'Cercedilla',
+		    'texto' => 'Casas rurales en alquiler',
+	    ],
+	    [
+		    'titulo' => 'Deltebre',
+		    'texto' => 'Apartamentos en alquiler',
+	    ],
+	    [
+		    'titulo' => 'La Molina',
+		    'texto' => 'Casas en alquiler',
+	    ],
+	    [
+		    'titulo' => 'Landes',
+		    'texto' => 'Alquileres vacacionales'
+	    ],
+    ];
+
+    $escapadas_ocultas = [
+	    [
+		    'titulo' => 'London',
+		    'texto' => 'Casas en alquiler'
+	    ],
+	    [
+		    'titulo' => 'Mandello del Lario',
+		    'texto' => 'Alquileres vacacionales'
+	    ],
+	    [
+		    'titulo' => 'Motril',
+		    'texto' => 'Apartamentos en alquiler'
+	    ],
+	    [
+		    'titulo' => 'Orbaneja del Castillo',
+		    'texto' => 'Alquileres vacacionales'
+	    ],
+    ];
+
+?>
+
 <div class="h-[600px]"></div>
+
+<!-- Inspiraciones -->
+<div class="py-12 px-6 bg-[#F7F7F7]">
+    <h2 class="text-xl font-semibold">
+        Inspiración para futuras escapadas
+    </h2>
+
+    <div class="my-6 border-b border-gray-300 flex flex-nowrap items-center gap-6 overflow-x-auto no-scrollbar">
+		<?php foreach ( $escapadas_tipos as $index => $escapada ) : ?>
+            <div class="whitespace-nowrap pb-3 border-b-2 <?php echo $index === 0 ? 'border-black' : 'border-transparent'; ?>">
+                <p class="mt-1 text-sm font-semibold <?php echo $index !== 0 ? 'text-gray-500' : ''; ?>">
+					<?php echo $escapada; ?>
+                </p>
+            </div>
+		<?php endforeach; ?>
+    </div>
+
+    <div x-data="{footerEscapadas: false}">
+        <div class="grid grid-cols-2 gap-y-6 gap-x-4 text-sm">
+		    <?php foreach ( $escapadas as $index => $escapada ) : ?>
+                <?php if ( $index === 7 ) : ?>
+                    <div :class="footerEscapadas === true ? 'block' : 'hidden'">
+                <?php else : ?>
+                    <div>
+                <?php endif; ?>
+                    <h3><?php echo $escapada['titulo']; ?></h3>
+                    <p class="text-[#717171]"><?php echo $escapada['texto']; ?></p>
+                </div>
+		    <?php endforeach; ?>
+
+            <button class="flex items-center gap-1 font-semibold" @click="footerEscapadas = true"
+                :class="footerEscapadas === true ? 'hidden' : ''">
+                Mostrar más
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" data-slot="icon" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+            </button>
+        </div>
+
+        <div class="mt-6 grid grid-cols-2 gap-y-6 gap-x-4 text-sm" x-show="footerEscapadas">
+		    <?php foreach ( $escapadas_ocultas as $escapada ) : ?>
+                <div>
+                    <h3><?php echo $escapada['titulo']; ?></h3>
+                    <p class="text-[#717171]"><?php echo $escapada['texto']; ?></p>
+                </div>
+		    <?php endforeach; ?>
+        </div>
+    </div>
+
+</div>
+<!-- end Inspiraciones -->
 
 <footer class="px-6 bg-[#F7F7F7] border-t border-gray-300 divide-y divide-gray-300">
 	<div class="divide-y divide-gray-300">
